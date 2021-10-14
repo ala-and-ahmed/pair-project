5bc0a88b28acccf9926f2f2f0f928b708f0ec815
aladin-jridi <aladinjridi007@gmail.com>
2021-10-14 11:54:16 +0100

    merging

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ function cont(){
         }if(counter===0){
             document.getElementById('count').innerHTML ='your time is over'
              $('body').append('<audio autoplay><source src="./song/explosion-sound-effect-10-seconds.mp3" type="audio/mp3"> </audio>')
-            
         }
        
     },1000);}
